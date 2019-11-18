634f19e59fb409d18092cf33f61e5ee1f75ee61c
Grzegorz <51416884+koanaj@users.noreply.github.com>
2019-11-19 00:49:34 +0100

    1 Commit

---
+++ fibonacci.py
@@ -0,0 +1,18 @@
+def fibo(n):
+    if n<0: 
+        print("musi byc powyzej 0") 
+    elif n==1: 
+        return 0
+    elif n==2: 
+        return 1
+    else: 
+        return fibo(n-1)+fibo(n-2)
+print('Wpisz liczbe ciagu fibonacciego:')
+
+n = input()
+
+try:
+    liczba = int(n)
+    print('wynik wynosi: ',fibo(int((n))))
+except ValueError:
+    print('podano wartosc string')
